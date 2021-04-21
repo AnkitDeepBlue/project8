1b4f5486357365f4c5037bf984c670eaa2053c75
Ankit Tripathi <ankit.deepblue@gmail.com>
2021-04-21 22:48:12 +0530

    Project 9 as 8

---
+++ pageClasses/loginPageClass.py
@@ -0,0 +1,57 @@
+import time
+
+from allure_commons.types import AttachmentType
+from selenium.webdriver.support.wait import WebDriverWait
+import random
+from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
+import allure
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from Locaters.login import *
+from SuperClasses.Logs import loggingClass
+from SuperClasses.Reusable_methods import reusableMethods
+
+logger = loggingClass.logGen()
+
+
+class loginPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def access_url(self):
+
+        self.driver.get("https://www.saucedemo.com/")
+
+    def enter_userName(self, username):
+        logger.info("Entering user name as >>"+ username)
+        self.driver.find_element(*addToCart.username).send_keys(username)
+
+    def enter_password(self, password):
+        logger.info("Entering password as >>"+ password)
+        self.driver.find_element(*addToCart.password).send_keys(password)
+
+    def clickLogin(self):
+        logger.info("Clicking login button")
+        self.driver.find_element(*addToCart.loginbtn).click()
+
+    def verifyTittle(self, title):
+        logger.info("Verifing title with " + title)
+        gettitle=self.driver.title
+
+        if gettitle != title:
+            logger.info("Title not matched")
+            assert False
+        else:
+            logger.info("Title matched")
+
+    def clickLogout(self):
+        logger.info("Clicking Menu button >> clicking logout")
+        time.sleep(2)
+        self.driver.find_element(*addToCart.menu).click()
+        self.driver.find_element(*addToCart.logout).click()
+
+
+
+
---
+++ testCaces/test_login.py
@@ -0,0 +1,34 @@
+import pytest
+
+from SuperClasses.baseClass import baseclass
+from SuperClasses.xls import myexel
+from pageClasses.loginPageClass import *
+
+
+class TestLogin(baseclass):
+
+    @pytest.mark.parametrize("userid, password",[("standard_user","secret_sauce"),("standard_user","secret_sauce")])
+    def test_loginFun(self, userid, password):
+        loginPage.access_url(self)
+        loginPage.enter_userName(self, userid)
+        loginPage.enter_password(self, password)
+        loginPage.clickLogin(self)
+        loginPage.clickLogout(self)
+        loginPage.verifyTittle(self, "Swag Labs")
+
+    @pytest.mark.skip
+    def test_loginFromXLS(self):
+        path = "D:\Pycharm\Myexcel\details.xlsx"
+        max_row = myexel.maxrow(self, path, "Sheet1")
+        max_col = myexel.maxcol(self, path, "Sheet1")
+        for r in range(2, max_row + 1):
+            for c in range(2, max_col + 1):
+                data_u = myexel.getCellValue(self, path, r, 1)
+                data_p = myexel.getCellValue(self, path, c, 2)
+                loginPage.enter_userName(self, data_u)
+                loginPage.enter_password(self, data_p)
+                loginPage.clickLogin(self)
+                loginPage.clickLogout(self)
+                loginPage.verifyTittle(self, "Swag Labs")
+
+
